dc2d9db0615002c1d06d342c699fb150c85f0d09
EUGENEPC\Eugene <e.tabahov@gmail.com>
2024-09-23 12:12:16 +0300

    Правки к SSR

--- project/main/urls.py
+++ project/main/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.home, name='home_url'),
     path('habitat/<int:id>/', views.habitat, name='habitat_url'),
     path('basket/<int:id>/', views.basket, name='basket_url'),
-    path('search/', views.search, name='search_url'),
 ]
